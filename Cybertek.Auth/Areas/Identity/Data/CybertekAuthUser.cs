c160fd7367ec2c31ef66b8ba22a14fe954f43235
sharabel22@yahoo.com <sharabel22@yahoo.com>
2022-05-28 04:40:59 +0800

    Add project files.

---
+++ Cybertek.Auth/Areas/Identity/Data/CybertekAuthUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Cybertek.Auth.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the CybertekAuthUser class
+    public class CybertekAuthUser : IdentityUser
+    {
+    }
+}
---
+++ Cybertek.Auth/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Cybertek.Auth.Areas.Identity.Data;
+using Cybertek.Auth.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Cybertek.Auth.Areas.Identity.IdentityHostingStartup))]
+namespace Cybertek.Auth.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CybertekAuthContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CybertekAuthContextConnection")));
+
+                services.AddDefaultIdentity<CybertekAuthUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<CybertekAuthContext>();
+            });
+        }
+    }
+}
